69253966a1334ddc657e01935726c9515c1f6ebd
graziacrp <graziacarpineta25@gmail.com>
2022-11-10 14:22:07 +0100

    ok

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let chilometri = prompt ("Inserisci i chilometri da percorrere");
 let età = prompt ("Inserisci la tua età");
+let prezzoTotaleDelViaggio = prompt ("Inserisci il prezzo totale del viaggio");
+
+
 
